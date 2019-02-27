576aa15c3d991e1d4d8eb12c6c5a991e17b9fab1
Connor Braa <connor.braa@gmail.com>
2019-02-26 20:33:43 -0800

    slow bft

--- epi_judge_cpp/tree_level_order.cc
+++ epi_judge_cpp/tree_level_order.cc
@@ -1,14 +1,41 @@
 #include <memory>
 #include <vector>
+#include <list>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 using std::vector;
+using std::list;
 
-vector<vector<int>> BinaryTreeDepthOrder(
-    const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+std::ostream& operator<<(std::ostream& os, const list<int>& dt) {
+  os << "[";
+  for (auto i : dt) os << i << ", ";
+  os << "]";
+  return os;
+}
+
+using t = const unique_ptr<BinaryTreeNode<int>>;
+list<int> LevelN(t& tree, int n) {
+  if (tree == nullptr) {
+    return {};
+  }
+  if (n == 0) {
+    return {tree->data};
+  }
+  auto rez = LevelN(tree->left, n - 1);
+  rez.splice(rez.end(), LevelN(tree->right, n - 1));
+  return rez;
+}
+
+vector<vector<int>> BinaryTreeDepthOrder(t& tree) {
+  vector<vector<int>> rez {};
+  auto level = LevelN(tree, 0);
+  int i = 1;
+  while (!level.empty()) {
+    rez.push_back(vector<int>{level.begin(), level.end()});
+    level = LevelN(tree, i++);
+  }
+  return rez;
 }
 
 int main(int argc, char* argv[]) {
